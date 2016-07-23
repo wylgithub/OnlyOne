e09e3f2d6b4484518eeca57683b953124367c539
Administrator <Administrator@PC-20150112PHQA>
2016-07-23 23:33:07 +0800

    添加Hibernate并测试成功！

---
+++ src/main/java/com/testssh2/junit/TestJunit4.java
@@ -0,0 +1,13 @@
+package com.testssh2.junit;
+/** 
+ * @author  Administrator: 
+ * @version 创建时间：2016-7-23 下午11:12:31 
+ * 类说明 
+ */
+public class TestJunit4 {
+	
+	public void testJunit()
+	{
+		System.out.println("Test Junit!");
+	}
+}
---
+++ src/test/java/com/testshh2/junit/TestJunit.java
@@ -0,0 +1,21 @@
+package com.testshh2.junit;
+
+import org.junit.Test;
+
+/** 
+ * @author  Administrator: 
+ * @version 创建时间：2016-7-23 下午11:14:15 
+ * 类说明 
+ */
+public class TestJunit {
+
+	@Test
+	public void testJ()
+	{
+		/*TestJunit t = new TestJunit();
+		
+		t.testJ();*/
+		
+		System.out.println("hello world！");
+	}
+}
